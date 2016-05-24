92c9f37f201ce846c563ecef2414293fe405087b
foomin10 <foomin10@gmail.com>
2016-05-24 21:41:19 +0900

    Add

--- js/app.js
+++ js/app.js
@@ -7,11 +7,21 @@ function main(input) {
 
 function listen(url) {
   const output = document.querySelector('#output');
+  const time = Date.now();
   
   const text = `
-    <div class="bc">
-      <div>${url}</div>
+    <div class="bc bc_${time}">
+      <div class="url">${url}</div>
       <audio src="${url}" controls autoplay>
     </div>`;
+  
   output.insertAdjacentHTML('beforeend', text);
+  
+  const bc = output.querySelector(`.bc_${id}`);
+  const audio = bc.querySelector('audio');
+  const url_s = bc.querySelector('.url');
+  
+  audio.addEventListener('error', (event)=>{
+    url_s.classList.add('del');
+  });
 }
